813435be352f7a2c2e3deeacbd6702749b2ebf70
WashingtonYandun <washington.yandun06@gmail.com>
2022-05-22 23:48:48 -0500

    start making the components

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+const Todo = () => {
+    return (
+        <div className="todoContainer">
+            <form>
+                <input type="submit" value="TODO"></input>
+            </form>
+        </div>
+    );
+};
+
+export default Todo;
